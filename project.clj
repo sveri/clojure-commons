93936177a9854a8cebe973cdb4be38aaa62c2484
sveri <sveri@sveri.de>
2014-07-16 23:29:01 +0200

    added test.check dep again

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.sveri/clojure-commons "0.1.2"
+(defproject de.sveri/clojure-commons "0.1.3"
   :description "A clojure helper library for my personal projects."
   :url "https://github.com/sveri/clojure-commons"
   :license {:name "Eclipse Public License"
