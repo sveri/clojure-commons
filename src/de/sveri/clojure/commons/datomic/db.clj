5bf092ed04c33409db204c260a24a5c48d027a31
sveri <sveri@sveri.de>
2014-07-25 22:14:21 +0200

    more convenience db functions

--- src/de/sveri/clojure/commons/datomic/db.clj
+++ src/de/sveri/clojure/commons/datomic/db.clj
@@ -22,11 +22,44 @@
   [db-val column-query]
   (d/q column-query db-val))
 
-(defn get-by-id-lazy-ref [db-val id]
+(defn get-by-id-lazy-ref
   "Returns a datom by just touching it, every reference inside it will be kept by id."
+  [db-val id]
   (d/touch (d/entity db-val id)))
 
+(defn retrieve-additional-datoms
+  "Fetches the attribute identified by add-key
+  from entity and puts it back into the map.
+  Additionally the id of the entity is merged into the map"
+  [db-val entity add-key]
+  (let [entity-m (into {} entity)]
+    (assoc
+        (assoc entity-m add-key (map #(d/touch (d/entity db-val (:db/id %))) (seq (add-key entity-m))))
+      :db/id (:db/id entity))))
+
+
 ; inserts
+(defn get-or-create-entity
+  "Retrieves the id of an entity defined by it's key through it's value.
+  Entity has to be unique.
+  It still works if entity is not unique, but expect unexpected behavior then."
+  [db-conn part-id entity-key entity-val & [data-map]]
+  {:pre [(and (keyword? entity-key)(keyword? part-id))]}
+  (let [id (ffirst (find-by-attr-and-search-string (d/db db-conn) entity-key entity-val))]
+    (or id
+        (let [temp_id (d/tempid part-id)
+              transact-data (merge {:db/id temp_id entity-key entity-val} data-map)
+              tx @(d/transact db-conn [transact-data])]
+          (d/resolve-tempid (d/db db-conn) (:tempids tx) temp_id)))))
+
+(defn add-multi-ref-entity-to-entity
+  "Function to add refs to an entity.
+  Takes an additional data map for that ref and adds it to the new entity.
+  If entity exists already it remains unchanged."
+  [db-conn partition-id upsert-ref ref-key entity-key entity-val user-name & [data-map]]
+  (let [ex-id (get-or-create-entity db-conn partition-id entity-key entity-val data-map)]
+    @(d/transact db-conn [{:db/id [upsert-ref user-name] ref-key ex-id}])))
+
 (defn create-entity
   "Returns a vector that can be inserted into datomic. Adds a temporary :db/id to the given data-map."
   [partition-id data-map]
