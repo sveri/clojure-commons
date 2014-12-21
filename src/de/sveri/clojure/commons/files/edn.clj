e598527b2d7011bf8469e157073cf440cb1103b0
sveri <sveri@sveri.de>
2014-12-21 20:14:48 +0100

    bugfix in edn

--- src/de/sveri/clojure/commons/files/edn.clj
+++ src/de/sveri/clojure/commons/files/edn.clj
@@ -1,10 +1,29 @@
 (ns de.sveri.clojure.commons.files.edn
-  ;(:import (java.io PushbackReader))
   (:require [clojure.java.io :as io]
-            ;[clojure.edn :as edn]
-            ))
+            [clojure.edn :as edn]))
 
-(defn from-edn
+(defn from-edn [fname]
   "reads an edn file from classpath"
-  [fname]
-  (read-string (slurp (io/resource fname))))
+  (with-open [rdr (-> (io/resource fname)
+                      io/reader
+                      java.io.PushbackReader.)]
+    (edn/read rdr)))
+
+
+;(ns de.sveri.clojure.commons.files.edn
+;  (:import (java.io PushbackReader))
+;  (:require [clojure.java.io :as io]
+;            [clojure.edn :as edn]
+;            ))
+
+;(defn from-edn
+;  "reads an edn file from classpath"
+;  [fname]
+;  (read-string (slurp (io/resource fname))))
+
+;(defn from-edn [fname]
+;  "reads an edn file from classpath"
+;  (with-open [rdr (-> (io/resource fname)
+;                      io/reader
+;                      java.io.PushbackReader.)]
+;    (edn/read rdr)))
