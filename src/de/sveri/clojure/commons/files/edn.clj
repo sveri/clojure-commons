553f91ca69344bde5684ccf538d098f1a2909dd9
sveri <sveri80@gmail.com>
2015-03-31 16:53:49 +0200

    adding new edn function

--- src/de/sveri/clojure/commons/files/edn.clj
+++ src/de/sveri/clojure/commons/files/edn.clj
@@ -1,13 +1,22 @@
 (ns de.sveri.clojure.commons.files.edn
   (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn])
+  (:import (java.io PushbackReader)))
 
 (defn from-edn [fname]
   "reads an edn file from classpath"
   (with-open [rdr (-> (io/resource fname)
                       io/reader
-                      java.io.PushbackReader.)]
+                      PushbackReader.)]
     (edn/read rdr)))
+
+(defn filepath->edn [fp]
+  "Reads a file from the given path and returns it's edn data"
+  (-> fp
+      (io/file)
+      (io/reader)
+      (PushbackReader.)
+      (edn/read)))
 
 
 ;(ns de.sveri.clojure.commons.files.edn
