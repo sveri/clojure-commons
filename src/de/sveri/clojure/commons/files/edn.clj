437bd4a2a015f43e656916dcd3649a6275212043
sveri <sveri80@gmail.com>
2015-04-04 10:54:31 +0200

    typify it!

--- src/de/sveri/clojure/commons/files/edn.clj
+++ src/de/sveri/clojure/commons/files/edn.clj
@@ -1,8 +1,15 @@
 (ns de.sveri.clojure.commons.files.edn
   (:require [clojure.java.io :as io]
-            [clojure.edn :as edn])
-  (:import (java.io PushbackReader)))
+            [clojure.edn :as edn]
+            [clojure.core.typed :refer [ann] :as t]
+            [de.sveri.ctanns.clojure-core])
+  (:import (java.io PushbackReader Reader)
+           (java.net URL)))
 
+(ann from-edn [String -> t/Any])
+(ann ^:no-check clojure.java.io/resource [String -> URL])
+(ann ^:no-check clojure.java.io/reader [URL -> Reader])
+(ann ^:no-check clojure.edn/read [PushbackReader -> t/Any])
 (defn from-edn [fname]
   "reads an edn file from classpath"
   (with-open [rdr (-> (io/resource fname)
@@ -10,6 +17,7 @@
                       PushbackReader.)]
     (edn/read rdr)))
 
+(ann filepath->edn [String -> t/Any])
 (defn filepath->edn [fp]
   "Reads a file from the given path and returns it's edn data"
   (-> fp
--- src/de/sveri/clojure/commons/files/faf.clj
+++ src/de/sveri/clojure/commons/files/faf.clj
@@ -1,6 +1,8 @@
 (ns de.sveri.clojure.commons.files.faf
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.core.typed :as t]
+            [de.sveri.ctanns.clojure-core]))
 
+(t/ann create-if-not-exists [String -> boolean])
 (defn create-if-not-exists [folder]
-  (when-not (.exists (io/file folder))
-    (.mkdirs (io/file folder))))
+  (or (.exists (io/file folder)) (.mkdirs (io/file folder))))
