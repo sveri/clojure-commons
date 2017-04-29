4cf274aa453e278588936d3c4b7e1dafd76f2a4f
sveri <sveri@sveri.de>
2017-04-29 15:54:48 +0200

    adding function to log an exception in a good way

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject de.sveri/clojure-commons "0.2.0"
+(defproject de.sveri/clojure-commons "0.2.1"
   :description "A clojure helper library for my personal projects."
   :url "https://github.com/sveri/clojure-commons"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
     :dependencies [[org.clojure/test.check "0.5.8"]
-                   [org.clojure/core.typed "0.2.84"]]
+                   [org.clojure/core.typed "0.2.84"]
+                   [org.clojure/tools.logging "0.3.1"]]
   :deploy-repositories [["clojars-self" {:url           "https://clojars.org/repo"
                                          :sign-releases false}]]
   :source-paths ["src"])
---
+++ src/de/sveri/clojure/commons/log.clj
@@ -0,0 +1,30 @@
+(ns de.sveri.clojure.commons.log
+  (:require [clojure.tools.logging :as log])
+  (:import (java.io StringWriter PrintWriter)))
+
+
+(defn error->string [e]
+  (let [sw (new StringWriter)
+        pw (new PrintWriter sw)
+        e-string (.printStackTrace e pw)]
+    (.toString sw)))
+
+
+
+(defn trace [e]
+  (log/trace (error->string e)))
+
+(defn debug [e]
+  (log/debug (error->string e)))
+
+(defn info [e]
+  (log/info (error->string e)))
+
+(defn warn [e]
+  (log/warn (error->string e)))
+
+(defn error [e]
+  (log/error (error->string e)))
+
+(defn fatal [e]
+  (log/fatal (error->string e)))
